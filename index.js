618a73beae5ee646b556636af1f0b9cad53b79a7
Eric McDowell <emcdow123@gmail.com>
2017-12-14 22:35:28 -0500

    trying to connect to DB

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+const express = require('express');
+const path = require('path');
+const PORT = process.env.PORT || 5000;
+
+var pg = require('pg');
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+
+app.get('/db', function (request, response) {
+  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+    client.query('SELECT * FROM test_table', function(err, result) {
+      dont();
+      if (err) {
+        console.error(err); response.send("Error " + err);
+      } else {
+        response.render('pages/db', {
+          results: result.rows
+        });
+      }
+    })
+  })
+})

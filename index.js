1664fa8417275f268cb33b4d61c54196ac974f35
Eric McDowell <emcdow123@gmail.com>
2017-12-14 22:45:21 -0500

    Pull from the exisiting orders table

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ express()
 
 app.get('/db', function (request, response) {
   pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-    client.query('SELECT * FROM test_table', function(err, result) {
+    client.query('SELECT * FROM orders', function(err, result) {
       dont();
       if (err) {
         console.error(err); response.send("Error " + err);
